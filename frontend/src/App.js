0c1eef3b278667c27d7a0ac01544d66228e962c0
Tushar <tusharrikhe95@gmail.com>
2022-04-28 19:29:29 +0530

    Video-03-Create-React-App

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+function App() {
+  return (
+    <div className="App">
+      <header>
+        <a href='/'>Amazon</a>
+      </header>
+      <main>List Product</main>
+
+    </div>
+  );
+}
+
+export default App;
